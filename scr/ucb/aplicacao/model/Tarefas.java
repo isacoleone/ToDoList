4b04c654d7aef5fbb38cc125c6bf8565ffbc16bb
daviwai11 <davimonsueth10@gmail.com>
2025-09-01 20:26:14 -0300

    teste

--- scr/ucb/aplicacao/model/Tarefas.java
+++ scr/ucb/aplicacao/model/Tarefas.java
@@ -1,4 +1,34 @@
 package ucb.aplicacao.model;
 
 public class Tarefas {
+
+   private int id;
+    private String descricao;
+    private boolean concluida;
+
+    public Tarefas(int id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+        this.concluida = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    } 
+
+
 }
+
+
