2d171f3905736a101185ca5048ba25cb8a555458
daviwai11 <davimonsueth10@gmail.com>
2025-09-01 21:07:17 -0300

    testedavi

--- scr/ucb/aplicacao/model/Tarefas.java
+++ scr/ucb/aplicacao/model/Tarefas.java
@@ -2,33 +2,7 @@ package ucb.aplicacao.model;
 
 public class Tarefas {
 
-   private int id;
-    private String descricao;
-    private boolean concluida;
-
-    public Tarefas(int id, String descricao) {
-        this.id = id;
-        this.descricao = descricao;
-        this.concluida = false;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public boolean isConcluida() {
-        return concluida;
-    }
-
-    public void setConcluida(boolean concluida) {
-        System.out.println("Tarefa criada com sucesso!");
-
-        this.concluida = concluida;
-    } 
+   System.out.print("davi");
 
 } 
 
