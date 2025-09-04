9c23a832bb185e81daa25223f612168e8f00b54a
Ana Luísa Coleone <isacoleone27@gmail.com>
2025-09-03 21:12:39 -0300

    opcoes de menu feitas

--- scr/ucb/aplicacao/cli/AppToDoList.java
+++ scr/ucb/aplicacao/cli/AppToDoList.java
@@ -1,5 +1,30 @@
 package ucb.aplicacao.cli.ToDoList.scr.ucb.aplicacao.cli;
 
-public class AppToDoList {
+import java.util.Scanner;
 
+public class AppToDoList {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("-----------MENU-----------");
+        System.out.println("| Opção 1 - Criar Tarefa");
+        System.out.println("| Opção 2 - Listar Tarefas");
+        System.out.println("| Opção 3 - Atualizar Tarefa");
+        System.out.println("| Opção 4 - Remover Tarefa");
+        System.out.println("| Opção 5 - Pesquisar Tarefas");
+        System.out.println("| Opção 6 - Sair");
+        System.out.print("Escolhe uma opção: ");
+
+        int opcao = entrada.nextInt();
+
+        switch (opcao) {
+            case 1:
+                break;
+
+            case 2:
+                break;
+
+        }
+
+    }
 }
--- scr/ucb/aplicacao/service/TarefaService.java
+++ scr/ucb/aplicacao/service/TarefaService.java
@@ -1,4 +1,11 @@
 package ucb.aplicacao.cli.ToDoList.scr.ucb.aplicacao.service;
 
+import ucb.aplicacao.model.Tarefas;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TarefaService {
+    private List<Tarefas> tarefas = new ArrayList<>();
+
+
 }
