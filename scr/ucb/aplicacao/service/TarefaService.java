450687a9b9d4a7a214c72fe75bd01b2a1db3b4c2
Lumminus <contaprojetos24@gmail.com>
2025-09-06 12:47:19 -0300

    Criada estrutura basica para a listagem

--- scr/ucb/aplicacao/service/TarefaService.java
+++ scr/ucb/aplicacao/service/TarefaService.java
@@ -1,4 +1,17 @@
 package ucb.aplicacao.cli.ToDoList.scr.ucb.aplicacao.service;
 
+import ucb.aplicacao.model.Tarefas;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
 public class TarefaService {
+    private List<Tarefas> ListaTarefas; //Cria lista de tarefas
+    private long NovoId; //Armazena os IDs
+
+    public TarefaService() { // Objeto que cria a lista de tarefas
+       this.ListaTarefas = new ArrayList<>();
+       this.NovoId = 1; // Obriga a tarefa a nunca ter 0 como ID
+   }
 }
