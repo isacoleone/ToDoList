181695eadf733e70e61e0492b8540a8bf9400d22
Lumminus <contaprojetos24@gmail.com>
2025-09-06 13:19:49 -0300

    Criação da listagem de todos os itens da lista

--- scr/ucb/aplicacao/service/TarefaService.java
+++ scr/ucb/aplicacao/service/TarefaService.java
@@ -10,8 +10,17 @@ public class TarefaService {
     private List<Tarefas> ListaTarefas; //Cria lista de tarefas
     private long NovoId; //Armazena os IDs
 
+    //CRIAÇÃO DA LISTA DE TAREFAS
+
     public TarefaService() { // Objeto que cria a lista de tarefas
        this.ListaTarefas = new ArrayList<>();
        this.NovoId = 1; // Obriga a tarefa a nunca ter 0 como ID
    }
+
+   //LISTAGEM TOTAL DA LISTA DE TAREFAS
+
+   public List<Tarefas> exibirLista(){
+        return new ArrayList<> (ListaTarefas); //Cria uma copia da lista de tarefas para exibição
+   }
 }
+
