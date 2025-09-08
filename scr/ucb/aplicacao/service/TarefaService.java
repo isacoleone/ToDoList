144795d17893a96272a2961ce7cb81a9d99a78b1
André Marcos <contaprojetos24@gmail.com>
2025-09-08 19:22:23 -0300

    Update TarefaService.java
    Criei a listagem total das tarefas onde:
    
    Eu crio uma copia da lista ja criada anteriormente
    Exibo essa lista para o usuario
    
    AJUSTADO DE ACORDO COM A BRANCH "DEVELOP"

--- scr/ucb/aplicacao/service/TarefaService.java
+++ scr/ucb/aplicacao/service/TarefaService.java
@@ -1,12 +1,13 @@
 package ucb.aplicacao.service;
 
 import ucb.aplicacao.model.Tarefas;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class TarefaService {
     List<Tarefas> listaTarefas= new ArrayList<Tarefas>(); // declaração do array para armazenar as tarefas
+
+    //CRIAÇÃO DA TAREFA
 
     public String criarTarefa(String titulo, String descricao){
         Tarefas tarefa = new Tarefas(titulo, descricao); // instância de Tarefas
@@ -16,5 +17,11 @@ public class TarefaService {
         String mensagem = "Sua tarefa foi adicionada com sucesso!";
 
         return mensagem;
+    }
+
+    //LISTAGEM TOTAL DA LISTA DE TAREFAS
+
+    public List<Tarefas> exibirLista(){
+        return new ArrayList<> (listaTarefas); //Cria uma copia da lista de tarefas para exibição
     }
 }
