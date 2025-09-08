cef2552b7942498e1b8454e28bb1f5c4ae95599f
André Marcos <contaprojetos24@gmail.com>
2025-09-08 19:16:37 -0300

    Update TarefaService.java
    Ajustes

--- scr/ucb/aplicacao/service/TarefaService.java
+++ scr/ucb/aplicacao/service/TarefaService.java
@@ -1,36 +1,10 @@
 package ucb.aplicacao.service;
 
 import ucb.aplicacao.model.Tarefas;
-
 import java.util.ArrayList;
-import java.util.List;
-
-import ucb.aplicacao.model.Tarefas;
-
-import java.util.ArrayList;
-
 import java.util.List;
 
 public class TarefaService {
-<<<<<<< Updated upstream
-    private List<Tarefas> ListaTarefas; //Cria lista de tarefas
-    private long NovoId; //Armazena os IDs
-
-    //CRIAÇÃO DA LISTA DE TAREFAS
-
-    public TarefaService() { // Objeto que cria a lista de tarefas
-       this.ListaTarefas = new ArrayList<>();
-       this.NovoId = 1; // Obriga a tarefa a nunca ter 0 como ID
-   }
-
-   //LISTAGEM TOTAL DA LISTA DE TAREFAS
-
-   public List<Tarefas> exibirLista(){
-        return new ArrayList<> (ListaTarefas); //Cria uma copia da lista de tarefas para exibição
-   }
-}
-
-=======
     List<Tarefas> listaTarefas= new ArrayList<Tarefas>(); // declaração do array para armazenar as tarefas
 
     //CRIAÇÃO DA TAREFA
@@ -51,4 +25,3 @@ public class TarefaService {
         return new ArrayList<> (listaTarefas); //Cria uma copia da lista de tarefas para exibição
     }
 }
->>>>>>> Stashed changes
