9329a3f9a439c3a7e81d2951a25161361e8af99c
Andre Atlas <andrealvesacioli@gmail.com>
2025-09-07 21:07:46 -0300

    Criação do metodo AtuliazarTarefas

--- scr/ucb/aplicacao/service/TarefaService.java
+++ scr/ucb/aplicacao/service/TarefaService.java
@@ -7,9 +7,12 @@ import java.util.List;
 
 public class TarefaService {
     List<Tarefas> listaTarefas= new ArrayList<Tarefas>(); // declaração do array para armazenar as tarefas
-
+    private long proximoId = 1;
+    
     public String criarTarefa(String titulo, String descricao){
         Tarefas tarefa = new Tarefas(titulo, descricao); // instância de Tarefas
+
+        tarefa.setID(proximoId++);
 
         listaTarefas.add(tarefa);
 
@@ -17,4 +20,19 @@ public class TarefaService {
 
         return mensagem;
     }
+
+    public String atualizarTarefa(long id, String novoTitulo, String novaDescricao, boolean novaStatus) {
+    
+    for (Tarefas tarefa : listaTarefas) {
+        if (tarefa.getID() == id) {
+            
+            tarefa.setTitulo(novoTitulo);
+            tarefa.setDescricao(novaDescricao);
+            tarefa.setCompleta(novaStatus);
+            return "Tarefa com ID " + id + " atualizada com sucesso.";
+        }
+    }
+    
+    return "Erro: Tarefa com ID " + id + " não encontrada.";
+}
 }
