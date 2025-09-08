793479f87b77ad156129495fc104c181f2c0da60
Lumminus <contaprojetos24@gmail.com>
2025-09-08 19:13:49 -0300

    alinhamento da branch com o projeto principal

--- scr/ucb/aplicacao/service/TarefaService.java
+++ scr/ucb/aplicacao/service/TarefaService.java
@@ -1,4 +1,9 @@
-package ucb.aplicacao.cli.ToDoList.scr.ucb.aplicacao.service;
+package ucb.aplicacao.service;
+
+import ucb.aplicacao.model.Tarefas;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import ucb.aplicacao.model.Tarefas;
 
@@ -7,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TarefaService {
+<<<<<<< Updated upstream
     private List<Tarefas> ListaTarefas; //Cria lista de tarefas
     private long NovoId; //Armazena os IDs
 
@@ -24,3 +30,25 @@ public class TarefaService {
    }
 }
 
+=======
+    List<Tarefas> listaTarefas= new ArrayList<Tarefas>(); // declaração do array para armazenar as tarefas
+
+    //CRIAÇÃO DA TAREFA
+
+    public String criarTarefa(String titulo, String descricao){
+        Tarefas tarefa = new Tarefas(titulo, descricao); // instância de Tarefas
+
+        listaTarefas.add(tarefa);
+
+        String mensagem = "Sua tarefa foi adicionada com sucesso!";
+
+        return mensagem;
+    }
+
+    //LISTAGEM TOTAL DA LISTA DE TAREFAS
+
+    public List<Tarefas> exibirLista(){
+        return new ArrayList<> (listaTarefas); //Cria uma copia da lista de tarefas para exibição
+    }
+}
+>>>>>>> Stashed changes
